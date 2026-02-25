72ffaf94c2d95bcaa887bfeef8bae016c8b8ecd0
陨落基围虾 <3161880837@qq.com>
2026-02-25 17:13:56 +0800

    build: 将构建工具从tsc迁移到tsup
    修改构建配置以使用tsup替代tsc，添加tsup配置文件并更新package.json中的脚本。同时调整logo.svg的导出方式以兼容新的构建工具。

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,1 @@
-export { default as Logo } from "./logo.svg";
+export * as logo from "./logo.svg";
---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    splitting: false,
+    sourcemap: true,
+    dts: true,
+    clean: true,
+    minify: true,
+    loader: {
+        ".svg": "dataurl"
+    },
+});
