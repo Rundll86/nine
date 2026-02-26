dd320339ab38b17656cdbb12f1df6b6568f244da
陨落基围虾 <3161880837@qq.com>
2026-02-26 14:50:28 +0800

    style(test): 添加空行以提高代码可读性

--- src/test.ts
+++ src/test.ts
@@ -2,6 +2,7 @@ import { wrap } from "./dom";
 import examples from "./examples";
 
 const value = wrap(0);
+
 examples.Selector({ value })
     .on("select", e => console.log("当前选项：", e))
     .on("toggleState", e => console.log("当前是否开关：", e))
