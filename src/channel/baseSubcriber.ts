96eac86e58f47a366070abe3e4ae13295a44be43
陨落基围虾 <3161880837@qq.com>
2026-03-29 10:35:31 +0800

    refactor(channel): 修改订阅者类以支持返回值收集
    修改BaseSubcriber和EventSubcriber类，添加泛型参数R用于返回值类型
    emit方法现在返回回调结果的数组，便于收集所有订阅者的返回值
    保持原有功能的同时增强灵活性

--- src/channel/baseSubcriber.ts
+++ src/channel/baseSubcriber.ts
@@ -1,22 +1,20 @@
-export interface SubcriberCallback<T extends unknown[]> {
-    (...data: T): void;
+export interface SubcriberCallback<T extends unknown[], R> {
+    (...data: T): R;
 }
-export interface Subcriber<T extends unknown[]> {
-    callback: SubcriberCallback<T>;
+export interface Subcriber<T extends unknown[], R> {
+    callback: SubcriberCallback<T, R>;
     once: boolean;
 }
-export class BaseSubcriber<T extends unknown[]> {
-    protected subcribers: Subcriber<T>[] = [];
+export class BaseSubcriber<T extends unknown[], R = void> {
+    protected subcribers: Subcriber<T, R>[] = [];
 
-    subcribe(callback: SubcriberCallback<T>, once: boolean = false): void {
+    subcribe(callback: SubcriberCallback<T, R>, once: boolean = false): void {
         this.subcribers.push({
             callback,
             once,
         });
     };
-    emit(...data: T): void {
-        for (const subcriber of this.subcribers) {
-            subcriber.callback(...data);
-        }
+    emit(...data: T): R[] {
+        return this.subcribers.map(subcriber => subcriber.callback(...data));
     }
 }
--- src/channel/eventSubcriber.ts
+++ src/channel/eventSubcriber.ts
@@ -1,24 +1,25 @@
 import { BaseSubcriber } from "./baseSubcriber";
 
-export class EventSubcriber<T extends unknown[]> extends BaseSubcriber<T> {
-    public parent?: BaseSubcriber<T>;
+export class EventSubcriber<T extends unknown[], R = void> extends BaseSubcriber<T, R> {
+    public parent?: BaseSubcriber<T, R>;
     public bubbleable: boolean;
     private emitting: boolean = false;
 
     constructor(config?: {
         bubbleable?: boolean;
-    }, parent?: BaseSubcriber<T>) {
+    }, parent?: BaseSubcriber<T, R>) {
         super();
         this.parent = parent;
         this.bubbleable = config?.bubbleable ?? false;
     }
-    emit(...data: T): void {
-        if (this.emitting) return;
+    emit(...data: T): R[] {
+        if (this.emitting) return [];
         this.emitting = true;
-        super.emit(...data);
+        const result = super.emit(...data);
         if (this.parent) {
             this.parent.emit(...data);
         }
         this.emitting = false;
+        return result;
     }
 }
